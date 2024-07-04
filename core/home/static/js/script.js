98339f78aae88f47a897a1ee7faa241c954deee6
Vedant <vedantkumbhar973@gmail.com>
2024-07-04 16:08:47 +0530

    newCommit

--- core/home/static/js/script.js
+++
@@ -1,5 +0,0 @@
-function togglePasswordVisibility(id) {
-    const passwordInput = document.getElementById(id);
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-}
